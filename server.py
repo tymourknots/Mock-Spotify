d02c73befc873e376cb8248771fc89afb255e930
tymourknots <taa2146@columbia.edu>
2023-11-19 22:19:29 -0500

    debugging

--- server.py
+++ server.py
@@ -14,12 +14,9 @@ from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response, abort, session
 
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
-
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
+app.secret_key = os.urandom(24)
 
 #
 # The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
